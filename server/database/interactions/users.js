f937f473dda9e83e0537507a4219b5899ab663f6
Tom <tom.divers9@gmail.com>
2022-01-31 11:30:13 +0000

    added getUserInfoFromEmail (not tested yet)

--- server/database/interactions/users.js
+++ server/database/interactions/users.js
@@ -5,11 +5,11 @@ const saltRounds = 10; //10 should be enough
 const pool = require('../db');
 
 //Query to add user to users table and returns randomly-generated userID
-const registerUserQuery = 'INSERT INTO users VALUES (DEFAULT, $1, FALSE, $2, $3, $4) RETURNING userID';
+const registerUserQuery = 'INSERT INTO users VALUES (DEFAULT, $1, FALSE, $2, $3, $4, $5) RETURNING userID';
 
 //Queries to add mentee/mentor to mentee/mentor table
 const registerMenteeQuery = 'INSERT INTO mentee VALUES ($1)';
-const registerMentorQuery = 'INSERT INTO mentor VALUES ($1, NULL, 0)';
+const registerMentorQuery = 'INSERT INTO mentor VALUES ($1)';
 
 /** 
  * Add a new user to the users table and mentee/mentor table
@@ -19,18 +19,18 @@ const registerMentorQuery = 'INSERT INTO mentor VALUES ($1, NULL, 0)';
  * @param {string} password User's plaintext password
  * @param {string} businessArea User's business area
  * @param {string} userType User's type ('mentee', 'mentor' or 'both')
+ * @param {boolean} emailsAllowed True if user gives us permission to send them emails, false otherwise
  * 
  * @throws {EmailAlreadyUsedError} Fails if email is already associated with another account 
  */
-async function registerUser(email, name, password, businessArea, userType) {    
+async function registerUser(email, name, password, businessArea, userType, emailsAllowed) {    
     //Generate password hash
     const hash = await bcrypt.hash(password, saltRounds)
 
-    var result;
-
     //Execute query that adds user to users table
+    var result;
     try {
-        result = await pool.query(registerUserQuery, [email, name, hash, businessArea]);
+        result = await pool.query(registerUserQuery, [email, name, hash, businessArea, emailsAllowed]);
     } catch(err) {
         //Throw appropriate error if email is already used
         if ((err.code === '23505') && (err.constraint === 'users_email_key'))
@@ -63,35 +63,44 @@ const findPasswordQuery = 'SELECT password FROM users WHERE email = $1';
 /**
  * Checks if email and password match. Returns promise containing true if so, false otherwise
  * 
- * @param {string} email 
- * @param {string} password 
+ * @param {string} email User's email
+ * @param {string} password User's password
+ * 
+ * @throws {UserNotFoundError} Fails if it can't find a user with given email
  */
-async function checkEmailAndPassword(email, password) {
-    var hash;
-    
+async function checkEmailAndPassword(email, password) {    
     //Find the password's hash
     const result = await pool.query(findPasswordQuery, [email]);
 
     if (result.rowCount === 0)
         throw {name: 'UserNotFoundError', message: `Could not find user ${email}`}
 
-    hash = result.rows[0].password;
+    const hash = result.rows[0].password;
 
     //Compare hash to input password
     return bcrypt.compare(password, hash);
 }
 
+//Query to find a user's information given their email
+const findUserFromEmailQuery = 'SELECT * FROM users WHERE email = $1';
 
+/**
+ * Returns all user information of user with a given email.
+ * 
+ * @param {string} email 
+ * 
+ * @throws {UserNotFoundError} Fails if it can't find a user with given email
+ */
+async function getUserInfoFromEmail(email) {
+    const result = await pool.query(findUserFromEmailQuery, [email]);
 
-exports.registerUser = registerUser;
-exports.checkEmailAndPassword = checkEmailAndPassword;
+    if (result.rowCount === 0)
+        throw {name: 'UserNotFoundError', message: `Could not find user ${email}`}
 
-//Informal Testing:
-async function main() {
-    await registerUser('bobjim@gmail.com', 'Bob Jimson', 'password', 'area51', 'both');
-    
-    correct = await checkEmailAndPassword('bobjim@gmail.com', 'password');
-    console.log(correct);
+    return result.rows[0];
 }
 
-main();
+//Module exports:
+exports.registerUser = registerUser;
+exports.checkEmailAndPassword = checkEmailAndPassword;
+exports.getUserInfoFromEmail = getUserInfoFromEmail;
