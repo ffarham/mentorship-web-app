735407fb1fa0451837c43b0142e14aa764918bbf
Farham Ahmad <55412683+FaMiFisH@users.noreply.github.com>
2022-02-03 19:45:15 +0000

    Delete client directory
    Removing next.js app.

--- client/pages/_app.tsx
+++
@@ -1,8 +0,0 @@
-// This wil wrap every single page.
-import type { AppProps } from 'next/app'
-
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
-
-export default MyApp
