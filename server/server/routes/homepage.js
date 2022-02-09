7fb1923332f2a20f9088e0eabf5e16cc2b2a44c9
n-telford <ntelford@protonmail.com>
2022-02-09 14:31:02 +0000

    database and server connected

--- server/server/routes/homepage.js
+++ server/server/routes/homepage.js
@@ -1,7 +1,8 @@
+const pool = require("../db");
+
 const router = require("express").Router();
 
 // ADD ROUTES HERE
-
 // e.g.
 // router.post("sendData...", async (req, res) => {
 //     try{
@@ -12,13 +13,20 @@ const router = require("express").Router();
 //     }
 // });
 
+const findPasswordQuery = 'SELECT password FROM users;'
+
 router.get("/", async (req, res) => {
     res.redirect("/home");
 });
 
 router.get("/home", async (req, res) =>{
     try{
-        res.send("This is the homepage");
+        const result = await pool.query(findPasswordQuery);
+        if(result.rowCount === 0){
+            throw {name:'Error :('};
+        }    
+        //"This is the homepage" + 
+        res.send(result.rows);
     }catch(err){
         console.log(err);
         res.status(500).send("Server Error");
