3aa8eb524d0e0d5465fafec83291cef6ede8ddbf
Farham Ahmad <farham329@gmail.com>
2022-02-25 19:10:02 +0000

    plans of actions page (complete)

---
+++ client/src/components/plan-of-action/PlanOfActionPanel.js
@@ -0,0 +1,121 @@
+import React, { useState, useEffect } from 'react';
+import {
+    Container,
+    Button,
+    Modal
+} from 'reactstrap';
+
+import api from "../../api/api";
+import PlanOfAction from "./PlanOfAction";
+import MileStonesPanel from "../milestones/MileStonesPanel";
+
+function PlanOfActionPanel() {
+
+    // get user information
+    const authState = JSON.parse(localStorage.getItem('authState'));
+    const userID = authState.userID;
+    const userType = authState.userType;
+
+    const [planOfActions, setPlanOfActions] = useState([]);
+    // get all of user's plans of actions
+    useEffect(() => {
+        const url = `/api/v1/plan-of-actions/${userID}/${userType}`;
+        api.get(url).then(
+            (res) => {
+                if(res.data){
+                    setPlanOfActions(res.data);
+                }
+            },
+            // runs when response status falls out of 2xx
+            (error) => {
+                console.log(error);
+            }
+        );
+    }, []);
+
+    const [activePoA, setActivePoA] = useState({});
+    const [popUp, setPopUp] = useState(false);
+
+    const handleClick = (planOfAction) => {
+        setActivePoA(planOfAction);
+        setPopUp(true);
+    };
+
+    const markComplete = () => {
+        api.post("/api/v1/markPOAcomplete", activePoA.id).then(
+            (res) => {
+                
+            }
+        );
+    }
+
+
+    return (
+        <> 
+            <div className="text-center my-4">
+                <h3 className="display-3 mb-0">Plan of Action</h3>
+            </div>
+            <Container fluid="xl" className="mx-9">
+                {planOfActions.map( (planOfAction) => {
+                    return (
+                        <div onClick={() => handleClick(planOfAction)}>
+                            <PlanOfAction data={planOfAction} />
+                        </div>
+                    );
+                }
+                
+            )}
+            <Modal
+              className="modal-dialog-centered"
+              isOpen={popUp}
+              toggle={() => setPopUp(false)}
+            >
+              <div className="modal-header">
+                <h6 className="modal-title" id="modal-title-default">
+                  {activePoA.planName}
+                </h6>
+                <button
+                  aria-label="Close"
+                  className="close"
+                  data-dismiss="modal"
+                  type="button"
+                  onClick={() => setPopUp(false)}
+                >
+                  <span aria-hidden={true}>×</span>
+                </button>
+              </div>
+
+              <div className="modal-body">
+                <p>
+                    {activePoA.planDescription}
+                </p>
+                <MileStonesPanel data={activePoA.milestones}/>            
+              </div>
+
+              <div className="modal-footer">
+                  {userType === "mentee" 
+                  ? <></>
+                  : <Button 
+                        color="primary" 
+                        type="button"
+                        onClick={markComplete}>
+                        Mark as Complete
+                    </Button> 
+                  }
+                <Button
+                  className="ml-auto"
+                  color="link"
+                  data-dismiss="modal"
+                  type="button"
+                  onClick={() => setPopUp(false)}
+                >
+                  Close
+                </Button>
+              </div>
+            </Modal>
+            </Container>
+        </>
+    );
+}
+
+export default PlanOfActionPanel;
