a360cc23d5cbdc7f715d7f723ea7cdd8b723a51d
Tom <tom.divers9@gmail.com>
2022-03-12 08:37:16 +0000

    renaming topic to meetingName

--- backend/server/routes/meetings.js
+++ backend/server/routes/meetings.js
@@ -38,10 +38,8 @@ router.post('/createGroupMeeting', checkAuth, async (req, res, next) => {
 
         console.log("/createGroupMeeting\n" + req.body);
 
-        var meetingName = req.body.meetingName;
-
         //Add the new meeting to the database
-        const makeMeetingResult = await pool.query('INSERT INTO groupMeeting VALUES (DEFAULT, $1, $2, NOW(), $3, $4, $5, $6, FALSE, $7) RETURNING groupMeetingID', [meetingName, req.userInfo.userID, req.body.meetingStart, req.body.meetingDuration, req.body.meetingType, req.body.place, req.body.meetingDescription]);
+        const makeMeetingResult = await pool.query('INSERT INTO groupMeeting VALUES (DEFAULT, $1, $2, NOW(), $3, $4, $5, $6, FALSE, $7) RETURNING groupMeetingID', [req.body.meetingName, req.userInfo.userID, req.body.meetingStart, req.body.meetingDuration, req.body.meetingType, req.body.place, req.body.meetingDescription]);
 
         //Get the ID of the new meeting
         const groupMeetingID = makeMeetingResult.rows[0].groupmeetingid;
@@ -57,10 +55,10 @@ router.post('/createGroupMeeting', checkAuth, async (req, res, next) => {
             notificationMessage = `${req.userInfo.name} is running a group meeting.`
         } else if (req.body.meetingType === 'workshop') {
             //Pull the IDs of users interested in what the workshop is about
-            interestedUsersResult = await pool.query('SELECT users.userID FROM users INNER JOIN interest ON users.userID = interest.userID WHERE interest.interest = $1', [req.body.topic]);
+            interestedUsersResult = await pool.query('SELECT users.userID FROM users INNER JOIN interest ON users.userID = interest.userID WHERE interest.interest = $1', [req.body.meetingName]);
 
             //Pick a notification message
-            notificationMessage = `${req.userInfo.name} is running a workshop about ${req.body.topic}.`
+            notificationMessage = `${req.userInfo.name} is running a workshop about ${req.body.meetingName}.`
         }
 
         //Notify each user and add record them as an attendee
