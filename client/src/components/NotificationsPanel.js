9a604c181797983ec2a90056bce6ab275da33743
TNelsQn <81658190+TNelsQn@users.noreply.github.com>
2022-03-12 00:11:22 +0000

    notifications panel

--- client/src/components/NotificationsPanel.js
+++ client/src/components/NotificationsPanel.js
@@ -1,49 +1,56 @@
-import React, { useState, useEffect, useContext } from "react";
+import React, { useState, useEffect } from 'react';
 import {
-    Card
+    Card,
+    Row,
+    Col,
+    Button,
+    Modal,
+    Badge
 } from 'reactstrap';
-
 
 import api from "../api/api";
 import Notification from "./Notification";
 
-
 function NotificationsPanel(){
 
-    // get user information
-    //const authState = JSON.parse(localStorage.getItem('authState'));
-    //const userID = authState.userID;
-    const userID = 1;
-
-    const [ notifications, setNotifications ] = useState([]);
-
-    // get all of user's notifications.
+    // get all users requests
+    const [notifications, setNotifications] = useState([]);
     useEffect(() => {
-      const url = `/api/v1/notifications/${userID}`;
-      api.get(url).then(
-          (res) => {
-              if(res.data){
-                  console.log(res.data);
-                  setNotifications(res.data);
-              }
-          },
-          // runs when response status falls out of 2xx
-          (error) => {
-              console.log(error);
-          }
-      );
-  }, []);
-  
-    return (
+        api.get(`/api/v1/notifications`).then(
+            (res) => {
+                if(res.data){
+                    setNotifications(res.data);
+                }
+            }
+        );
+    }, []);
 
-      <Card style={{padding:0, margin:0}}  className="bg-secondary shadow border-0 px-2 py-5">
-        <h3 >Notifications</h3>
-          {notifications.map((notification) => (
-              <Notification data={notification}/>
-              ))}
-      </Card>
+    return(
+        <>
+            <Card className="bg-secondary shadow border-0">
+                <Row className="mx-4 my-2"> 
+                    <div className="mt-2 mb-2">
+                        <h4 className="display-4 mb-0">Notifications</h4>
+                    </div>
+                </Row>
+                {/* <Row className="mx-4 my-2"> */}
+                {notifications.length === 0
+                    ? <div className="mb-3">
+                        You have no notifications.
+                    </div>
+                    : <div className="scrollView mb-3">
+                        {notifications.map( (n) => {
+                                return(
+                                    <div>
+                                        <Notification notification={n} />
+                                    </div>
+                        );})
+                        }
+                    </div>
+                    }
+            </Card>
+        </>
     );
-    
 }
 
 export default NotificationsPanel;
