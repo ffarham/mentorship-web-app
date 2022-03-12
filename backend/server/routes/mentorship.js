03b2d2329e01fa83b05d4b4b1766168e4927ef8b
Tom <tom.divers9@gmail.com>
2022-03-12 07:13:47 +0000

    extra cond on mentor individual meetings

--- backend/server/routes/mentorship.js
+++ backend/server/routes/mentorship.js
@@ -97,7 +97,7 @@ router.get('/meetings/meetings', checkAuth, async (req, res, next) => {
             (SELECT meeting.meetingID, 'meeting' AS meetingType, users.name, meeting.meetingName, meeting.meetingStart, meeting.meetingStart::VARCHAR AS startString, meeting.meetingDuration::VARCHAR AS durationString, meeting.place, meeting.confirmed, meeting.attended::INTEGER, meeting.description, meeting.mentorFeedback AS feedback, meeting.requestMessage
                 FROM meeting 
                 INNER JOIN users ON meeting.menteeID = users.userID 
-                WHERE meeting.mentorID = $1)
+                WHERE meeting.mentorID = $1 AND meeting.confirmed = \'true'\)
                 
             UNION 
             
